d20ce897d7c1746583a61bfb28a2de37e6b4a17f
Przemek Hendel <przem1361@gmail.com>
2017-01-10 15:03:57 +0000

    added a chair

--- artefact.py
+++ artefact.py
@@ -1,6 +1,15 @@
 ﻿import viz
 viz.go()
-a = viz.addChild('lab.osgb')
-ground = viz.addChild('eyeballs.osg')
+#a = viz.addChild('lab.osgb')
+#gallery = viz.addChild('gallery.osgb')
 sound =viz.addAudio('barber.mp3')
 sound.play()
+
+# Add a crosshair fixed to the view 
+crosshair = viz.add('beachball.osgb',pos=(0,0,0.1),euler = [0,-90,0], scale = [0.02,0.01,0.01])
+texture = viz.addTexture('crosshair.png')
+crosshair.texture(texture);
+crosshair.setReferenceFrame(viz.RF_VIEW)
+
+
+chair = viz.add('Chair.osgb', pos= (0,0,0), scale= [0.01,0.01,0.01])
